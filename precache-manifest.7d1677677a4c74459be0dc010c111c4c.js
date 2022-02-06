c7ffb3d2e6582a25f92bf06bb5bb8706a36e91d1
Abhijeet6394 <abhijeetkumar5714@gmail.com>
2022-02-06 19:26:20 +0530

    Updates

---
+++ precache-manifest.7d1677677a4c74459be0dc010c111c4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08a52789474d482b4584",
+    "url": "/formValidationReact/static/css/main.0e4d7201.chunk.css"
+  },
+  {
+    "revision": "08a52789474d482b4584",
+    "url": "/formValidationReact/static/js/main.10478845.chunk.js"
+  },
+  {
+    "revision": "49e29c92a169005fa4c1",
+    "url": "/formValidationReact/static/js/runtime~main.2608f90b.js"
+  },
+  {
+    "revision": "ee7f37ea120d5f770ab3",
+    "url": "/formValidationReact/static/js/2.18feb4ae.chunk.js"
+  },
+  {
+    "revision": "6cf08ee037da1eedaa49b291329ee202",
+    "url": "/formValidationReact/index.html"
+  }
+];
